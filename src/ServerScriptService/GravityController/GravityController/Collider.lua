4d64e9ff22f28bf4186aac24edd0ed75f8b80d5f
EgoMoose <EgoMoose@users.noreply.github.com>
2020-12-31 15:22:57 -0500

    Add better jump detector

--- src/ServerScriptService/GravityController/GravityController/Collider.lua
+++ src/ServerScriptService/GravityController/GravityController/Collider.lua
@@ -23,7 +23,7 @@ function ColliderClass.new(controller)
 
 	self.Model = Instance.new("Model")
 
-	local sphere, vForce, floor, gryo = create(self, controller)
+	local sphere, vForce, floor, floor2, gryo = create(self, controller)
 
 	self._maid = Maid.new()
 	
@@ -32,6 +32,7 @@ function ColliderClass.new(controller)
 	self.Sphere = sphere
 	self.VForce = vForce
 	self.FloorDetector = floor
+	self.JumpDetector = floor2
 	self.Gyro = gryo
 
 	init(self)
@@ -67,20 +68,19 @@ function create(self, controller)
 	sphere.Transparency = 1
 	sphere.CustomPhysicalProperties = CUSTOM_PHYSICAL
 
-	local sphere2 = Instance.new("Part")
-	sphere2.Name = "Sphere2"
-	sphere2.CanCollide = false
-	sphere2.Massless = true
-	sphere2.Size = Vector3.new(2.5, 2.5, 2.5)
-	sphere2.Shape = Enum.PartType.Ball
-	sphere2.Transparency = 1
-
 	local floor = Instance.new("Part")
 	floor.Name = "FloorDectector"
 	floor.CanCollide = false
 	floor.Massless = true
 	floor.Size = Vector3.new(2, 1, 1)
 	floor.Transparency = 1
+
+	local floor2 = Instance.new("Part")
+	floor2.Name = "JumpDectector"
+	floor2.CanCollide = false
+	floor2.Massless = true
+	floor2.Size = Vector3.new(2, 0.2, 1)
+	floor2.Transparency = 1
 
 	local weld = Instance.new("Weld")
 	weld.C0 = CFrame.new(0, -hipHeight, 0.1)
@@ -89,16 +89,16 @@ function create(self, controller)
 	weld.Parent = sphere
 
 	local weld = Instance.new("Weld")
-	weld.C0 = CFrame.new(0, -hipHeight, 0.1)
-	weld.Part0 = controller.HRP
-	weld.Part1 = sphere2
-	weld.Parent = sphere2
-
-	local weld = Instance.new("Weld")
 	weld.C0 = CFrame.new(0, -hipHeight - 1.5, 0)
 	weld.Part0 = controller.HRP
 	weld.Part1 = floor
 	weld.Parent = floor
+
+	local weld = Instance.new("Weld")
+	weld.C0 = CFrame.new(0, -hipHeight - 1.1, 0)
+	weld.Part0 = controller.HRP
+	weld.Part1 = floor2
+	weld.Parent = floor2
 
 	local vForce = Instance.new("VectorForce")
 	vForce.Force = Vector3.new(0, 0, 0)
@@ -113,14 +113,14 @@ function create(self, controller)
 	gyro.CFrame = controller.HRP.CFrame
 	gyro.Parent = controller.HRP
 
-	sphere2.Touched:Connect(function() end)
 	floor.Touched:Connect(function() end)
+	floor2.Touched:Connect(function() end)
 
 	sphere.Parent = self.Model
-	sphere2.Parent = self.Model
 	floor.Parent = self.Model
+	floor2.Parent = self.Model
 
-	return sphere2, vForce, floor, gyro
+	return sphere, vForce, floor, floor2, gyro
 end
 
 function init(self)
@@ -140,35 +140,10 @@ function ColliderClass:Update(force, cframe)
 end
 
 function ColliderClass:IsGrounded(isJumpCheck)
-	if not isJumpCheck then
-		local parts = self.FloorDetector:GetTouchingParts()
-		for _, part in pairs(parts) do
-			if not part:IsDescendantOf(self.Controller.Character) then
-				return true
-			end
-		end
-	elseif not self.Controller.StateTracker.Jumped then
-		local filter = {}
-		local parts = self.Sphere:GetTouchingParts()
-		for _, part in pairs(parts) do
-			if not part:IsDescendantOf(self.Controller.Character) then
-				table.insert(filter, part)
-			end
-		end
-
-		if #filter > 0 then
-			params.FilterDescendantsInstances = filter
-
-			local gravityUp = self.Controller._gravityUp
-			local result = workspace:Raycast(self.Sphere.Position, -10*gravityUp, params)
-
-			if result then
-				local max = math.cos(math.rad(self.Controller.Humanoid.MaxSlopeAngle))
-				local dot = result.Normal:Dot(gravityUp)
-				if max < dot then
-					return true
-				end
-			end
+	local parts = (isJumpCheck and self.JumpDetector or self.FloorDetector):GetTouchingParts()
+	for _, part in pairs(parts) do
+		if not part:IsDescendantOf(self.Controller.Character) then
+			return true
 		end
 	end
 end
